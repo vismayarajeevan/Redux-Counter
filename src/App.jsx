6391fd8e8fb50066d81c9b18909d2c74f84eed06
vismayarajeevan <vismayarajeevan15@gmail.com>
2025-04-22 11:38:29 +0530

    counter design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 
 import './App.css'
+import Counter from './components/Counter'
 
 function App() {
 
   return (
     <>
      <h1 className='text-center mt-5'>Counter App</h1>
+     <div className='d-flex align-items-center justify-content-center' >
+      <Counter />
+      </div>
+
     </>
   )
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Counter = () => {
   return (
-    <div>Counter</div>
+    <div className="border rounded p-5 text-center mt-5">
+    <h1 style={{fontSize:"100px"}}>0</h1>
+    
+     <div style={{width:'700px'}} className='d-flex justify-content-between my-5'>
+       <button className='btn btn-warning'  >Decrement</button>
+       <button className='btn btn-danger'  >Reset</button>
+       <button className='btn btn-success' >Increment</button>
+       </div>
+
+       <div className='d-flex justify-content-evenly align-items-center'>
+           <input type="text" placeholder='Increment counter amount' className='form-control'  />
+           <button className='btn btn-primary ms-3' >Increment by amount</button> 
+       </div>
+ 
+    </div>
   )
 }
 
