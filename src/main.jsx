a5242196a5428558c4edb5b723e80efaa5660051
vismayarajeevan <vismayarajeevan15@gmail.com>
2025-04-22 12:31:39 +0530

    redux package installed and store created

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from 'react-redux'
+import counterStore from './redux/counterStore.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={counterStore}>
+      <App />
+      </Provider>
   </StrictMode>,
 )
---
+++ src/redux/counterStore.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const counterStore = configureStore({
+    reducer:{
+
+    }
+})
+
+
+export default counterStore
