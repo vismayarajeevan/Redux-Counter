d1b8ed4e4c035367b6fe07bd85ab27d6aea92db2
vismayarajeevan <vismayarajeevan15@gmail.com>
2025-04-22 14:18:47 +0530

    increment function implemented

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment } from '../redux/counterSlice'
+
 
 const Counter = () => {
+
+const {count} = useSelector(state=>state.counterReducer)
+const dispatch = useDispatch()
+
   return (
     <div className="border rounded p-5 text-center mt-5">
-    <h1 style={{fontSize:"100px"}}>0</h1>
+    <h1 style={{fontSize:"100px"}}>{count}</h1>
     
      <div style={{width:'700px'}} className='d-flex justify-content-between my-5'>
        <button className='btn btn-warning'  >Decrement</button>
        <button className='btn btn-danger'  >Reset</button>
-       <button className='btn btn-success' >Increment</button>
+       <button className='btn btn-success' onClick={()=>dispatch(increment())} >Increment</button>
        </div>
 
        <div className='d-flex justify-content-evenly align-items-center'>
